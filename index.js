5984f8bc7a267973b3ae169e7d3435b0aa1a23ac
david01silva-d <davi01silva@hotmail.com>
2024-11-17 18:02:50 -0300

    Laços de repetição

---
+++ index.js
@@ -0,0 +1,34 @@
+// let numero = Number(prompt("Digite um numero"))
+
+// let i = 0
+
+// while (i<=10) {
+//     let resulTado = numero * i
+//     alert(`${numero} * ${i} = ${resulTado}`)
+//     i++
+// }
+
+// let Numero = Number(prompt("Digite um numero para calculo"))
+
+// for(let i = 0; i <= 10; i++){
+//     let resultado = Numero * i
+
+//     alert(`${Numero} * ${i} = ${resultado}`)
+
+// }
+
+
+
+let senha = Number(prompt("Digite sua senha"))
+
+while (senha != 1234) {
+    alert("Senha incorreta")
+
+    senha = Number(prompt("Por favor digite a senha correta"))   
+}
+
+alert(`Senha correta efetuando login....`)
+
+
+
+
